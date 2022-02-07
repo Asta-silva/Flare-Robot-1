e2b1d602a776f8e515f3a76debf457dba60e69db
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-02-07 17:40:01 +0530

    Update __main__.py

--- Flare_Robot/__main__.py
+++ Flare_Robot/__main__.py
@@ -265,7 +265,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_animation(
-            GROUP_START_IMG, caption= "ʜᴇʏ! ᴄᴜᴛᴇ ʀᴇᴍᴇᴍʙᴇʀ ɪs ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ!!!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            GROUP_START_IMG, caption= "ʜᴇʏ! ᴄᴜᴛᴇ ʀᴇᴍ ɪs ʜᴇʀᴇ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ!!!\n<b>Haven't slept since:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
